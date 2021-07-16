6cb961bf93066c0956b4f94b494dad8545a3695c
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2021-07-16 09:09:55 +0200

    added dynamic selection with vanilla js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var player = document.getElementById('player-iframe');
+var thumbnails = document.getElementsByClassName('video-thumbnail');
+
+for (var i = 0; i < thumbnails.length; i++) {
+  currentThumbnail = thumbnails[i];
+  currentThumbnail.addEventListener('click', setVideo);
+}
+
+function setVideo() {
+  console.log(this);
+  var videoSrc = this.getAttribute('data-src');
+  console.log(videoSrc);
+  player.setAttribute('src', videoSrc);
+}
